9e6464e22843e8a8a22bdb44f500090a2a48fe15
Bart <bart@taelemans.org>
2017-12-13 16:37:39 +0100

    Swiss/Norwegian toegevoegd + props-file aangepast met strings van nationaliteiten om values terug te vinden

---
+++ src/model/Norwegian.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Bart Taelemans
+ * @date 13 dec. 2017
+ * @project CurrencyRates
+ * @purpose 
+ *
+ */
+public class Norwegian extends Nationality
+{
+	public Norwegian() {
+		super.setNationality("Norwegian");
+		super.setLanguage("Norwegian");
+		super.setCurrency("NOK");
+	}
+}
---
+++ src/model/Swiss.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Bart Taelemans
+ * @date 13 dec. 2017
+ * @project CurrencyRates
+ * @purpose 
+ *
+ */
+public class Swiss extends Nationality
+{
+	public Swiss() {
+		super.setNationality("Swiss");
+		super.setLanguage("German");
+		super.setCurrency("CHF");
+	}
+}
